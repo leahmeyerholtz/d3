3dcb167bd2048d05c3380bcd9728e0b13a02dcf5
leahmeyerholtz <lmeyerholtz@gmail.com>
2014-06-18 23:06:27 -0400

    init

---
+++ d3Test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+
+  <!-- load all dat d3.js -->
+  <script src="http://d3js.org/d3.v3.min.js"></script>
+
+  <!-- include stylesheet -->
+  <link rel="stylesheet" type="text/css" href="http://localhost/css/d3style.css"> 
+</head>
+
+<body>
+  <svg class="circle"></svg>
+  
+  <!-- load your scripts at the end to prevent slow loading -->
+  <script type="text/javascript" src="http://localhost/js/d3js.js"></script>
+</body>
+</html>
